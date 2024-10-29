793417a66c537c11c5d2059e88524e5b2a2a7e1e
lanyangyangchifan <142667779+lanyangyangchifan@users.noreply.github.com>
2024-10-29 23:09:31 +0800

    Update emerge_score.py

--- DEGCN_pro/emerge_score.py
+++ DEGCN_pro/emerge_score.py
@@ -1,17 +1,16 @@
 import numpy as np
 import pickle
 
-# ./work_dir/test/test_A/75_8_test_score.pkl
+# file_name1 = './work_dir/test/test_A/75_85_test_score.pkl'
+# file_name2 = './work_dir/test/test_A/76_65_test_score.pkl'
+# file_name3 = './work_dir/test/test_A/74_95_test_score.pkl'
 file_name1 = './work_dir/test/test_B/75_85_test_score.pkl'
-# file_name2 = './work_dir/test/test_A/76_6_test_score.pkl'
 file_name3 = './work_dir/test/test_B/76_65_test_score.pkl'
-# file_name3 = './work_dir/test/test_A/75_8_test_score.pkl'
 file_name2 = './work_dir/test/test_B/74_95_test_score.pkl'
 with open(file_name1, 'rb') as file1, open(file_name2, 'rb') as file2, open(file_name3, 'rb') as file3:
     data_dict1 = pickle.load(file1)
     data_dict2 = pickle.load(file2)
     data_dict3 = pickle.load(file3)
-    # data_dict4 = pickle.load(file4)
 
 value_list1 = list(data_dict1.values())  # 提取字典的值
 array1 = np.array(value_list1)
@@ -19,15 +18,13 @@ value_list2 = list(data_dict2.values())  # 提取字典的值
 array2 = np.array(value_list2)
 value_list3 = list(data_dict3.values())  # 提取字典的值
 array3 = np.array(value_list3)
-# value_list4 = list(data_dict4.values())  # 提取字典的值
-# array4 = np.array(value_list4)
+
 # 计算加权平均数组
 weight1 = 3/11
 weight2 = 4/11
 weight3 = 4/11
-weight4 = 0.0
-weighted_average = weight1 * array1 + weight2 * array2 + weight3 * array3 + weight3 * array3 #+ weight4 * array4
-#0.3 0.4 0.4 相同位置的百分比： 77.8
+weighted_average = weight1 * array1 + weight2 * array2 + weight3 * array3 + weight3 * array3 
+
 #以下两个代码块根据需要注释一个
 
 # 测试集B保存置信度文件
