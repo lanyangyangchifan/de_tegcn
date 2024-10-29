b09c9792273b8aa9f2d319a0ccdf05080a76c131
lanyangyangchifan <142667779+lanyangyangchifan@users.noreply.github.com>
2024-10-29 09:34:07 +0800

    Add files via upload

---
+++ DEGCN_pro/data/data_denoise_3.py
@@ -0,0 +1,176 @@
+import numpy as np
+from tqdm import tqdm
+from rotation import *
+
+file_workdir = './data/'
+label_file = ['train_label', 'test_A_label']
+file = ['train_joint', 'test_A_joint', 'test_joint_B']  # 去0帧数据N,T,M,V,C
+file_suffix = '.npy'
+joint_validframe = [None] * 3
+joint_data = [None] * 3
+label_data = [None] * 2
+single_index = [None] * 3  # 单人动作标签
+double_index = [None] * 3  # 双人动作标签
+
+def load_fixed_data():
+    print('load_fixed_data')
+    data0 = np.load(file_workdir + 'fixed_data.npz')
+    joint_validframe[0] = np.array(data0['train_valid'])
+    joint_validframe[1] = np.array(data0['A_valid'])
+    joint_validframe[2] = np.array(data0['B_valid'])   
+    joint_data[0] = np.array(data0['x_train'])
+    joint_data[1] = np.array(data0['A_x'])
+    joint_data[2] = np.array(data0['B_x'])    
+    label_data[0] = np.array(data0['y_train'])
+    label_data[1] = np.array(data0['A_y'])   
+    single_index[0] = np.array(data0['train_single'])
+    single_index[1] = np.array(data0['A_single'])
+    single_index[2] = np.array(data0['B_single'])   
+    double_index[0] = np.array(data0['train_double'])
+    double_index[1] = np.array(data0['A_double'])
+    double_index[2] = np.array(data0['B_double'])    
+    data0.close()
+
+def rid_large_point():
+    print('rid_large_point')
+    mask = np.ones(joint_data[0].shape[0], dtype=bool)
+    for f in range(3):
+        for i in range(joint_data[f].shape[0]):
+            valid_frame = np.array([], dtype=int)
+            
+            for j in range(joint_validframe[f][i]):
+                max = np.max(np.abs(joint_data[f][i][j,0]))
+                if max <= 1000:
+                    valid_frame = np.concatenate((valid_frame, np.array([j], dtype=int)))
+
+            if len(valid_frame) == 0:
+                if f == 0:
+                    joint_validframe[f][i] = 0
+                    mask[i] = False
+                    continue
+                else:
+                    index = np.argwhere(joint_data[f][i][:,0]>1000)
+                    for t,v,c in index:
+                        joint_data[f][i][t,0,v,c] = 0
+            
+            for j in range(joint_validframe[f][i]):
+                if j not in valid_frame:
+                    absolute_diff = np.abs(valid_frame - j)
+                    closest_index = np.argmin(absolute_diff)
+                    closest_frame = valid_frame[closest_index]
+                    index = np.argwhere(np.abs(joint_data[f][i][j,0])>1000)
+                    for v,c in index:
+                        joint_data[f][i][j,0][v,c] = joint_data[f][i][closest_frame,0][v,c]
+                    valid_frame = np.concatenate((valid_frame, np.array([j], dtype=int)))
+
+        for i in double_index[f]:
+            valid_frame = np.array([], dtype=int)
+            
+            for j in range(joint_validframe[f][i]):
+                max = np.max(np.abs(joint_data[f][i][j,1]-joint_data[f][i][j,1][1]))
+                if max <= 1000:
+                    valid_frame = np.concatenate((valid_frame, np.array([j], dtype=int)))
+            
+            if len(valid_frame) == 0:
+                if f == 0:
+                    joint_validframe[f][i] = 0
+                    mask[i] = False
+                    continue
+                else:
+                    index = np.argwhere(joint_data[f][i][:,1]>1000)
+                    for t,v,c in index:
+                        joint_data[f][i][t,1,v,c] = 0
+            
+            for j in range(joint_validframe[f][i]):
+                if j not in valid_frame:
+                    absolute_diff = np.abs(valid_frame - j)
+                    closest_index = np.argmin(absolute_diff)
+                    closest_frame = valid_frame[closest_index]
+                    index = np.argwhere(np.abs(joint_data[f][i][j,1]-joint_data[f][i][j,1][1])>1000)
+                    for v,c in index:
+                        joint_data[f][i][j,1][v,c] = joint_data[f][i][closest_frame,1][v,c]
+                    valid_frame = np.concatenate((valid_frame, np.array([j], dtype=int)))   
+
+    joint_data[0] = joint_data[0][mask]
+    label_data[0] = label_data[0][mask]
+    joint_validframe[0] = joint_validframe[0][mask]
+
+
+def length_denoise_sample():
+    print('length_denoise_sample')
+    valid_frame_thre = 20 #11
+    mask = np.ones(len(joint_validframe[0]), dtype=bool)
+    N = len(joint_validframe[0])
+    for i in range(N):
+        if joint_validframe[0][i] < valid_frame_thre:
+            mask[i] = False
+    joint_data[0] = joint_data[0][mask]
+    label_data[0] = label_data[0][mask]
+    joint_validframe[0] = joint_validframe[0][mask]
+
+# def normalize():
+#     print('normalize')
+#     for f in range(3):
+#         joint_data[f] = joint_data[f]
+#         for i in range(joint_data[f].shape[0]):
+#             if joint_validframe[f][i] == 0:
+#                 continue    
+#             joint_data[f][i] = joint_data[f][i]/np.abs(joint_data[f][i]).max()
+def normalize():
+    for f in range(3):
+        joint_data[f] = joint_data[f].reshape(-1,300,34,3)
+        for i in range(joint_data[f].shape[0]):
+            mean_values = np.mean(joint_data[f][i], axis=(0,1))
+            joint_data[f][i] = joint_data[f][i]-mean_values
+            joint_data[f][i] = joint_data[f][i]/np.abs(joint_data[f][i]).max()
+
+def rotate_data(xaxis=[5, 6]):
+    for i in range(3):
+        N, T, M, V, C = joint_data[i].shape
+        s = np.transpose(joint_data[i], [0, 2, 1, 3, 4])  # N, T, M, V, C  to  N, M, T, V, C
+        # N, C, T, V, M = joint_data[i].shape
+        # s = np.transpose(joint_data[i], [0, 4, 2, 3, 1])  # N, C, T, V, M  to  N, M, T, V, C
+        for i_s, skeleton in enumerate(tqdm(s)):  # x轴对齐
+            if skeleton.sum() == 0:
+                continue
+            joint_rshoulder = skeleton[0, 0, xaxis[0]]
+            joint_lshoulder = skeleton[0, 0, xaxis[1]]
+            axis = np.cross(joint_rshoulder - joint_lshoulder, [1, 0, 0])
+            angle = angle_between(joint_rshoulder - joint_lshoulder, [1, 0, 0])
+            matrix_x = rotation_matrix(axis, angle)
+            for i_p, person in enumerate(skeleton):
+                if person.sum() == 0:
+                    continue
+                for i_f, frame in enumerate(person):
+                    if frame.sum() == 0:
+                        continue
+                    for i_j, joint in enumerate(frame):
+                        s[i_s, i_p, i_f, i_j] = np.dot(matrix_x, joint)
+
+        joint_data[i] = np.transpose(s, [0, 2, 1, 3, 4])
+
+def save_data():
+    print('save_data')
+    file_save_name_A = file_workdir + 'data' + '.npz'
+    file_save_name_B = file_workdir + 'B_data' + '.npz'
+
+    x_tr = np.array(joint_data[0]).reshape(-1, 300, 34*3)
+    print(x_tr.shape)
+    A_te = np.array(joint_data[1]).reshape(-1, 300, 34*3)
+    B_te = np.array(joint_data[2]).reshape(-1, 300, 34*3)
+    
+    y_tr = np.array(label_data[0])
+    y_A = np.array(label_data[1])
+    y_B = np.zeros((joint_data[2].shape[0], 155))
+    y_B[:, 0] = 1
+    
+    np.savez(file_save_name_A, x_train=x_tr, x_test=A_te, y_train=y_tr, y_test=y_A)
+    np.savez(file_save_name_B, x_train=x_tr, x_test=B_te, y_train=y_tr, y_test=y_B)
+
+if __name__ == '__main__':
+    load_fixed_data()
+    # rid_large_point() #去除大坐标噪点，可能无效
+    length_denoise_sample() #长度阈值去样本
+    normalize() #归一化
+    # rotate_data()
+    save_data()
