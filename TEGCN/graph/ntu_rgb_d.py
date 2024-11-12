e6f5f14c5c06cc2aec91872c08987d82247d899a
lanyangyangchifan <142667779+lanyangyangchifan@users.noreply.github.com>
2024-11-12 13:03:14 +0800

    Add files via upload

---
+++ TEGCN/graph/ntu_rgb_d.py
@@ -0,0 +1,45 @@
+import sys
+
+sys.path.extend(['../'])
+from graph import tools
+
+num_node = 17
+self_link = [(i, i) for i in range(num_node)]
+inward_ori_index = [(11, 9), (9, 7), (10, 8), (8, 6), (16, 14), 
+    (14, 12), (17, 15), (15, 13), (12, 6), (13, 7), 
+    (12, 13), (6, 7), (6, 1), (7, 2), (2, 1), 
+    (3, 1), (4, 2), (5, 3)]
+inward = [(i - 1, j - 1) for (i, j) in inward_ori_index]
+outward = [(j, i) for (i, j) in inward]
+neighbor = inward + outward
+
+
+class Graph:
+    def __init__(self, labeling_mode='spatial'):
+        self.A = self.get_adjacency_matrix(labeling_mode)
+        self.num_node = num_node
+        self.self_link = self_link
+        self.inward = inward
+        self.outward = outward
+        self.neighbor = neighbor
+
+    def get_adjacency_matrix(self, labeling_mode=None):
+        if labeling_mode is None:
+            return self.A
+        if labeling_mode == 'spatial':
+            A = tools.get_spatial_graph(num_node, self_link, inward, outward)
+        else:
+            raise ValueError()
+        return A
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    import os
+
+    # os.environ['DISPLAY'] = 'localhost:11.0'
+    A = Graph('spatial').get_adjacency_matrix()
+    for i in A:
+        plt.imshow(i, cmap='gray')
+        plt.show()
+    print(A)
